5b863e77884fe7b9d4f4ed4be589599f0f7a6b50
Franco Mariotti <fmariotti@fi.uba.ar>
2021-09-22 01:32:25 -0300

    version final actividad find lab fork

--- fork/find.c
+++ fork/find.c
@@ -1,47 +1,75 @@
+#define _GNU_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <dirent.h>
 #include <string.h>
+#include <stdbool.h>
+#include <fcntl.h>
 
-#define CURRENT_DIRECTORY "."
+#define MAX_PATH 251
+#define SEPARADOR "/"
+#define ROOT_DIRECTORY "."
 #define PREVIOUS_DIRECTORY ".."
 
-void printOcurrencies(DIR* folder,char* pattern/*,compare_function*/);
+typedef char* (*cmp_t)(const char*,const char* needle);
+void printOcurrencies(DIR* folder,char* path,char* pattern,cmp_t cmp);
 
 int
 main(int argc, char *argv[]) {
-    // Your code here
-    if (argc < 2) {
-		perror("cantidad de parametros incorrecta");
-		return -1;
+	int opt;
+	cmp_t ptr_cmp  = &strstr;
+    while ((opt = getopt(argc, argv, "i")) != -1) {
+    	switch (opt) {
+			case 'i':
+				ptr_cmp = &strcasestr;
+				break;
+			default:
+				fprintf(stderr, "Usage: %s [-i] pattern\n",
+						argv[0]);
+				_exit(EXIT_FAILURE);
+        }
+    }
+        
+    if (optind >= argc) {
+        perror("Expected argument after options");
+        _exit(EXIT_FAILURE);
     }
 
-    char* cadena = argv[1];
-    DIR* folder = opendir(CURRENT_DIRECTORY);
+    char* cadena = argv[optind];
+	char path[MAX_PATH];
+	memset(path,0,MAX_PATH);
+    DIR* folder = opendir(ROOT_DIRECTORY);
     if (!folder) {
 		perror("error en opendir");
-		return -1;
+		_exit(EXIT_FAILURE);
     }
 
-	printOcurrencies(folder,cadena);
+	printOcurrencies(folder,path,cadena,ptr_cmp);
 	closedir(folder);
-    return 0;
+    _exit(EXIT_SUCCESS);
 }
 
 
-void printOcurrencies(DIR* folder,char* pattern/*,compare_function*/) {
+void printOcurrencies(DIR* folder,char* path,char* pattern,cmp_t cmp) {
 	struct dirent *entry;
     while ((entry = readdir(folder))) {
-		char* ret = strstr(entry->d_name,pattern);
 
 		if (entry->d_type == DT_REG) {
+			char* ret = cmp(entry->d_name,pattern);
 			if (ret) {
-				printf("%s\n",entry->d_name);
+				printf("%s%s\n",path,entry->d_name);
 			}
 		} else if (entry->d_type == DT_DIR) {
-			if (ret) {
- 				printf("directorio: %s\n",entry->d_name);
-			}
-
+			if(strncmp(entry->d_name,ROOT_DIRECTORY,1) == 0 || strncmp(entry->d_name,PREVIOUS_DIRECTORY,2) == 0)
+				continue;
+			int fdRoot = dirfd(folder);
+			int fdSubdir = openat(fdRoot,entry->d_name,O_DIRECTORY);
+			DIR* subdir = fdopendir(fdSubdir);
+			strncat(path,entry->d_name,MAX_PATH-strlen(path)-1);
+			strncat(path,SEPARADOR,MAX_PATH-strlen(path)-1);
+			printOcurrencies(subdir,path,pattern,cmp);
+			closedir(subdir);
 		}
     }
 
