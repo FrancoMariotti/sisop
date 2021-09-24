624ea0a57eeb3e6ed7d48c74484557b890e0c414
Franco Mariotti <fmariotti@fi.uba.ar>
2021-09-24 12:18:05 -0300

    version final xargs

--- fork/xargs.c
+++ fork/xargs.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdbool.h>
+#include <sys/wait.h>
 
 #ifndef NARGS
 #define NARGs 4
@@ -11,8 +12,9 @@
 
 #define NEWLINE '\n'
 #define NULLCHAR '\0'
+#define BUFFERSIZE 1024
 
-size_t trimNewLine(char* str);
+int trimNewLine(char* str);
 
 int
 main(int argc, char *argv[]) {
@@ -20,39 +22,62 @@ main(int argc, char *argv[]) {
 		perror("cantidad de argumentos incorrecta");
 		_exit(EXIT_FAILURE);
 	}
+	
+	pid_t cpid;
 	char* command = argv[1];
-	printf("el comando leido es:%s\n",command);
-	char* linePtr = NULL;
+	char* line = NULL;
 	size_t len;
 	ssize_t nread = 0;
+	int argsRead = 1;
+	char argArr[NARGs][BUFFERSIZE]; 
+	char* args[NARGs + 2];
+	args[0] = command;
 
-	while(nread >= 0) {
-		for (size_t i = 0; i < NARGs; i++) {		
-			if ((nread = getline(&linePtr,&len,stdin)) != -1) {
-				len = trimNewLine(linePtr);
-				printf("linea leida: %s\n",linePtr);
+	while(argsRead > 0) {
+		argsRead = 0;
+		for (size_t i = 1; i <= NARGs; i++) {		
+			if ((nread = getline(&line,&len,stdin)) != -1) {
+				trimNewLine(line);
+				memset(argArr[i],0,BUFFERSIZE);
+				strncpy(argArr[i],line,nread);
+				args[i] = *(argArr + i);
+				argsRead ++;
 			}
 		}
 
-		//aca quedaria hacer un fork()
-		//que el hijo haga un execvp
-		//y el padre haga un wait.
-		//chequear errores y listo.
-		printf("no termine\n");
+		args[argsRead+1] = NULL;
+	
+		 if(argsRead > 0) {			
+			cpid = fork();
+			if (cpid < 0) {
+				perror("error en fork");
+				_exit(EXIT_FAILURE);
+			}
+
+			if (cpid == 0) {
+				execvp(command,args);
+				perror("error en exec");
+				_exit(EXIT_FAILURE);
+			} else {
+				if (wait(NULL) < 0) {
+					perror("error en wait");
+					_exit(EXIT_FAILURE);
+				}
+				printf("%c",NEWLINE);
+			}
+		} 
 	}
 
-
-	free(linePtr);
+	free(line);
 	_exit(EXIT_SUCCESS);
 }
 
-size_t trimNewLine(char* str) {
+int trimNewLine(char* str) {
 	char* ret;
 	ret = strchr(str,NEWLINE);
 	if (ret) {
 		*ret = NULLCHAR;
-		return strlen(str) - 1;
+		return 0;
 	}
-
-	return strlen(str);
+	return -1;
 }
